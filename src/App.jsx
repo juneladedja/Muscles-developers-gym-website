c5ee1e542be0e2ff9b89874ba67ad2023bcec0e9
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2024-03-05 11:01:12 +0100

    si

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { Herosection } from "./Herosection";
-import { Navbar } from "./Navbar";
-import { Gravity } from "./Gravity";
 import { Footer } from "./Footer";
 
 function App() {
   return (
     <>
-       <Navbar></Navbar>
-      <Herosection />
-      <Gravity /> 
       <Footer></Footer>
     </>
   );
